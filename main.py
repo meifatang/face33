2b27b112040439972c553d32e00440b59919673f
meifatang <me@tangmeifa.com>
2020-09-30 17:41:13 +0800

    operate exception

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ from sqlalchemy import create_engine
 def main():
     root = ET.parse('/Users/felix/Desktop/rep.xml').getroot()
 
-    df_cols = ['id', 'name', 'time']
+    df_cols = ['fdid', 'name', 'time']
     df = pd.DataFrame(columns=df_cols)
 
-    count = 1
     for i in root[5]:
         try:
-            df = df.append(
-                pd.Series([count, i[3][0][2].text, i[1].text], index=df_cols),
-                ignore_index=True)
+            if i[3][0][2].tag == '{http://www.hikvision.com/ver20/XMLSchema}name':
+                df = df.append(
+                    pd.Series([i[3][0][0].text, i[3][0][2].text, i[1].text], index=df_cols),
+                    ignore_index=True)
         except:
-            pass
-        count = count + 1
+            df = df.append(
+                pd.Series(['-', '-', '-'], index=df_cols),
+                ignore_index=True
+            )
 
-    print(df.head())
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df)
 
-    engine = create_engine('sqlite://')
-    df.to_sql('test', con=engine, if_exists="replace")
+    # engine = create_engine('sqlite://')
+    # df.to_sql('test', con=engine, if_exists="replace")
 
-    engine.execute('select * from test')
+    # engine.execute('select * from test')
 
 
 if __name__ == '__main__':
